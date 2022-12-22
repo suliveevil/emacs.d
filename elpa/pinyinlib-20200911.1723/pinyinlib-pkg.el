66ee7d5542d1cfaf2f82406283a3ecc020e34171
suliveevil <1361136657@qq.com>
2022-12-23 03:11:09 +0800

    avy + ace-pinyin

---
+++ elpa/pinyinlib-20200911.1723/pinyinlib-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from pinyinlib.el  -*- no-byte-compile: t -*-
+(define-package "pinyinlib" "20200911.1723" "Convert first letter of Pinyin to Simplified/Traditional Chinese characters" 'nil :commit "1772c79b6f319b26b6a394a8dda065be3ea4498d" :authors '(("Junpeng Qiu" . "qjpchmail@gmail.com")) :maintainer '("Junpeng Qiu" . "qjpchmail@gmail.com") :keywords '("extensions"))
