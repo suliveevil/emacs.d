22a2a1d4bea92c35542454a0e6674f011ce62360
suliveevil <1361136657@qq.com>
2022-12-23 17:33:27 +0800

    2022-12-23 17:33:26 +0800

--- elpa/vertico-0.29.0.20221221.225811/vertico-pkg.el
+++
@@ -1,2 +0,0 @@
-;; Generated package description from vertico.el  -*- no-byte-compile: t -*-
-(define-package "vertico" "0.29.0.20221221.225811" "VERTical Interactive COmpletion" '((emacs "27.1")) :commit "25676de12bc19cb4cc6c159d04b108923c9045b0" :authors '(("Daniel Mendler" . "mail@daniel-mendler.de")) :maintainer '("Daniel Mendler" . "mail@daniel-mendler.de") :url "https://github.com/minad/vertico")
