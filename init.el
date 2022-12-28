c9e539f74c880eefafedb790232df00c008839af
suliveevil <1361136657@qq.com>
2022-12-28 22:56:01 +0800

    comment hierarchy

--- init.el
+++ init.el
@@ -1013,41 +1013,30 @@ Similar to `marginalia-annotate-symbol', but does not show symbol class."
                        (org-roam-node-id node)))))
     (format "[%d]" count)))
 ;;
-  (cl-defmethod org-roam-node-filetitle ((node org-roam-node))
-    "Return the file TITLE for the node."
-    (if-let ((file (org-roam-node-file node)))
-        (with-temp-buffer
-          (insert-file-contents file nil 0 1024)
-          (cadr (assoc "TITLE"
-                       (org-collect-keywords (list "TITLE")))))
-      (cadr (assoc "TITLE"
-                   (org-collect-keywords (list "TITLE"))))))
+;;   (cl-defmethod org-roam-node-filetitle ((node org-roam-node))
+;;     "Return the file TITLE for the node."
+;;     (if-let ((file (org-roam-node-file node)))
+;;         (with-temp-buffer
+;;           (insert-file-contents file nil 0 1024)
+;;           (cadr (assoc "TITLE"
+;;                        (org-collect-keywords (list "TITLE")))))
+;;       (cadr (assoc "TITLE"
+;;                    (org-collect-keywords (list "TITLE"))))))
 
-(cl-defmethod org-roam-node-hierarchy ((node org-roam-node))
-  "Return the hierarchy for the node."
-  (let ((title (org-roam-node-title node))
-        (olp (org-roam-node-olp node))
-        (level (org-roam-node-level node))
-        (filetitle (org-roam-node-filetitle node)))
-    (concat
-     (if (> level 0) (concat filetitle " > "))
-     (if (> level 1) (concat (string-join olp " > ") " > "))
-     title))
-  )
+;; (cl-defmethod org-roam-node-hierarchy ((node org-roam-node))
+;;   "Return the hierarchy for the node."
+;;   (let ((title (org-roam-node-title node))
+;;         (olp (org-roam-node-olp node))
+;;         (level (org-roam-node-level node))
+;;         (filetitle (org-roam-node-filetitle node)))
+;;     (concat
+;;      (if (> level 0) (concat filetitle " > "))
+;;      (if (> level 1) (concat (string-join olp " > ") " > "))
+;;      title))
+;;   )
 ;;
 (setq org-roam-node-display-template
-      "${title:20} ${hierarchy:50} ${backlinkscount:5} ${tags:30} ${directories:15}")
-;;
-;; (setq org-roam-node-display-template (concat
-;;                                    "${type:8} ${backlinkscount:3} ${hierarchy:*}"
-;;                                   (propertize "${tags:20}" 'face 'org-tag) " "))
-;;
-;; (setq org-roam-node-display-template "${hierarchy:*} ${tags:20}")
-;;
-;; (setq org-roam-node-display-template
-;;       (concat "${title:*} "
-;;               (propertize "${tags:10}" 'face 'org-tag)
-;;               ))
+      "${title:30} ${backlinkscount:5} ${tags:30} ${directories:15}")
 ;; }}}
 
 ;; org-roam: completion
